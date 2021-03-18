fa040be45ded20098c7431264b049cfb8ad3287d
Hanh Nguyen <hanhnhk@gmail.com>
2021-03-18 15:02:20 -0500

    Complete the exercise

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state= {
+    inputValue: ''
+  }
+
+  handleChange= (event) =>{
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
   render() {
+    const {inputValue}= this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -11,9 +22,9 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" placeholder="Say Something" value={inputValue} onChange={this.handleChange}/>
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{inputValue}</p>
         </div>
       </div>
     );
